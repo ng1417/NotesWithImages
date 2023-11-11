69a8def20475f4e6a58d4ba2df66a04d9237d767
ng1417 <nicole.horner25@gmail.com>
2023-11-11 15:50:00 -0800

    taking a photo isn't saving or displaying

--- app/src/main/java/com/cs211d/noteswithdetailview/CreateFragment.kt
+++ app/src/main/java/com/cs211d/noteswithdetailview/CreateFragment.kt
@@ -1,6 +1,7 @@
 package com.cs211d.noteswithdetailview
 
 import android.content.ContentValues
+import android.content.Context
 import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -36,8 +37,10 @@ class CreateFragment : Fragment() {
     private lateinit var photoUri : Uri
     private lateinit var photoImageView: ImageView
     private lateinit var rootView : View
+    private var hasPhoto = false
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+            View? {
         rootView =  inflater.inflate(R.layout.fragment_create, container, false)
 
         val titleEditText : EditText = rootView.findViewById(R.id.enter_title_edittext)
@@ -61,6 +64,9 @@ class CreateFragment : Fragment() {
                     putString(getString(R.string.note_details_key), details)
 
                     // ***** YOUR STEP 2 PART C CODE HERE *****
+                    if(hasPhoto) {
+                        putParcelable(getString(R.string.photo_uri_key), photoUri)
+                    }
 
                 }
                 Navigation.findNavController(rootView)
@@ -82,8 +88,76 @@ class CreateFragment : Fragment() {
         // ***** YOUR STEP 2 PART B CODE HERE *****
         // You might have new variables you declare that are used in this method.
         // You should write other functions that are invoked from this method.
+        photoFile = createImageFile()
 
-
+        // Use requireContext() to get the context
+        val contextInFragment: Context = requireContext()
+        // create a content URI so the camera can access this app-specific file
+        val photoUri = FileProvider.getUriForFile(
+            contextInFragment,
+            "com.cs211d.noteswithdetailview.fileprovider", photoFile!!
+        )
+        takePictureResultLauncher.launch(photoUri)
     }
 
-}
+    private val takePictureResultLauncher: ActivityResultLauncher<Uri> =
+        registerForActivityResult( /* contract = */ ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                Log.d("ActivityResultLauncher", "TRUE")
+                displayImage()
+                savePhoto()
+
+            }
+        }
+    private fun createImageFile(): File {
+        // create a unique image filename
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        val imageFilename = "{$timeStamp}_photo.jpg"
+        Log.d("TEST", "in createImageFile, ${imageFilename}")
+        // get file path where the app can save a private image
+        val storageDir = rootView.context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        Log.d("image create path", "$storageDir , $imageFilename")
+        return File(storageDir, imageFilename)
+    }
+
+    private fun displayImage() {
+
+        //BitmapFactory.decodeFile(photoFile!!.absolutePath)
+        Log.d("TEST", "in displayImage, ${photoFile}")
+        val notePicture = BitmapFactory.decodeFile(photoFile!!.absolutePath)
+        Log.d("TEST", "in displayImage, notePicture = , ${notePicture}")
+        photoImageView.setImageBitmap(notePicture)
+     }
+
+    private fun savePhoto() {
+        Log.d("TEST", "in savePhotoClick ${photoFile}")
+
+        if (photoFile != null) {
+
+            // save image in background thread
+            CoroutineScope(Dispatchers.Main).launch {
+
+                val imageValues = ContentValues()
+                imageValues.put(MediaStore.MediaColumns.DISPLAY_NAME, photoFile!!.name)
+                imageValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    imageValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                val resolver = rootView.context.applicationContext.contentResolver
+                val uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageValues)
+
+                // save bitmap as JPEG
+                uri?.let {
+                    runCatching {
+                        resolver.openOutputStream(it).use { outStream ->
+                            val photoBitmap = BitmapFactory.decodeFile(photoFile!!.absolutePath, null)
+                            photoBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream!!)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    }
+
